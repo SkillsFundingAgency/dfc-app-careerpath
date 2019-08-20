80ceab43366dc81c4f1584c9ba2927b02a907044
Ian Crisp <icrisp@program-systems.co.uk>
2019-08-20 15:45:09 +0100

    Create integration tests for web app controller actions

---
+++ DFC.App.CareerPath.IntegrationTests/AutoMapperTests/AutoMapperProfileTests.cs
@@ -0,0 +1,47 @@
+using AutoMapper;
+using DFC.App.CareerPath.AutoMapperProfiles;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace DFC.App.CareerPath.IntegrationTests.ControllerTests
+{
+    [Trait("Category", "Integration")]
+    public class AutoMapperProfileTests : IClassFixture<CustomWebApplicationFactory<DFC.App.CareerPath.Startup>>
+    {
+
+        private readonly CustomWebApplicationFactory<DFC.App.CareerPath.Startup> factory;
+
+        public AutoMapperProfileTests(CustomWebApplicationFactory<DFC.App.CareerPath.Startup> factory)
+        {
+            this.factory = factory;
+        }
+
+        [Fact]
+        public void AutoMapperProfileConfigurationForCareerPathSegmentModelProfileReturnSuccess()
+        {
+            // Arrange
+            _ = factory.CreateClient();
+            var mapper = factory.Server.Host.Services.GetRequiredService<IMapper>();
+
+            // Act
+            mapper.ConfigurationProvider.AssertConfigurationIsValid<CareerPathSegmentModelProfile>();
+
+            // Assert
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void AutoMapperProfileConfigurationForAllProfilesReturnSuccess()
+        {
+            // Arrange
+            _ = factory.CreateClient();
+            var mapper = factory.Server.Host.Services.GetRequiredService<IMapper>();
+
+            // Act
+            mapper.ConfigurationProvider.AssertConfigurationIsValid();
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
--- DFC.App.CareerPath.IntegrationTests/ControllerTests/HomeControllerRouteTests.cs
+++ DFC.App.CareerPath.IntegrationTests/ControllerTests/HomeControllerRouteTests.cs
@@ -1,12 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace DFC.App.CareerPath.IntegrationTests.ControllerTests
 {
-    public class HomeControllerRouteTests
+    [Trait("Category", "Integration")]
+    public class HomeControllerRouteTests : IClassFixture<CustomWebApplicationFactory<DFC.App.CareerPath.Startup>>
     {
-        [Fact]
-        public void Test1()
+        private const string DefaultArticleName = "home-article";
+
+        private readonly CustomWebApplicationFactory<DFC.App.CareerPath.Startup> factory;
+
+        public HomeControllerRouteTests(CustomWebApplicationFactory<DFC.App.CareerPath.Startup> factory)
         {
+            this.factory = factory;
+
+            DataSeeding.SeedDefaultArticle(factory, DefaultArticleName);
+        }
+
+        public static IEnumerable<object[]> HomeContentRouteData => new List<object[]>
+        {
+            new object[] { "/Home/Error" },
+        };
+
+        [Theory]
+        [MemberData(nameof(HomeContentRouteData))]
+        public async Task GetHomeHtmlContentEndpointsReturnSuccessAndCorrectContentType(string url)
+        {
+            // Arrange
+            var uri = new Uri(url, UriKind.Relative);
+            var client = factory.CreateClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+
+            // Act
+            var response = await client.GetAsync(uri).ConfigureAwait(false);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal($"{MediaTypeNames.Text.Html}; charset={Encoding.UTF8.WebName}", response.Content.Headers.ContentType.ToString());
         }
     }
 }
