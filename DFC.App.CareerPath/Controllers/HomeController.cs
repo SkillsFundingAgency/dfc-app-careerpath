66478e5c43491fe8ec620d4535329489243f0758
Ian Crisp <icrisp@program-systems.co.uk>
2019-08-20 09:51:03 +0100

    User story DFC-10076 - Initial checking

---
+++ DFC.App.CareerPath/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using DFC.App.CareerPath.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DFC.App.CareerPath.Controllers
+{
+    public class HomeController : Controller
+    {
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
