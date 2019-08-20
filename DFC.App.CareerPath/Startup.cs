b8443a75e50ce238c0f94f541899d872b3960cba
Ian Crisp <icrisp@program-systems.co.uk>
2019-08-20 12:28:23 +0100

    Corrections to fix startup issue

--- DFC.App.CareerPath/Startup.cs
+++ DFC.App.CareerPath/Startup.cs
@@ -52,7 +52,7 @@ namespace DFC.App.CareerPath
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=Error}");
             });
         }
     }
