c39e75f488333122435f501ec4c34227c82a8b06
Ian Crisp <49161034+IanCrisp@users.noreply.github.com>
2019-08-21 16:21:50 +0100

    Dev story 10076 create app (#1)
    * Added code projects and solution structures
    
    * Added unit tests and integration test projects
    
    * Updated basic/default app settings templates
    
    * Updated GIT readme file
    
    * Corrections to fix startup issue
    
    * Added code to get a simple list and render in a simple view.
    Added code to get a simple item from a list link and render in a simple view.
    
    * Create integration tests for web app controller actions
    
    * Organised unit tests categories
    
    * Created unit tests for web app controller methods
    
    * Created unit tests for services methods
    
    * Added Ping action with associated tests
    
    * Remedy code analysis and style cop warnings
    
    * Remove obsolete code
    
    * Added logging
    
    * Remove unnecessary files
    Corrected spelling errors
    
    * Code tidy

---
+++ DFC.App.CareerPath.Data/Contracts/ICareerPathSegmentService.cs
@@ -0,0 +1,16 @@
+﻿using DFC.App.CareerPath.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DFC.App.CareerPath.Data.Contracts
+{
+    public interface ICareerPathSegmentService
+    {
+        Task<IEnumerable<CareerPathSegmentModel>> GetAllAsync();
+
+        Task<CareerPathSegmentModel> GetByIdAsync(Guid documentId);
+
+        Task<CareerPathSegmentModel> GetByNameAsync(string canonicalName, bool isDraft = false);
+    }
+}
---
+++ DFC.App.CareerPath/AutoMapperProfiles/CareerPathSegmentModelProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using DFC.App.CareerPath.Data.Models;
+using DFC.App.CareerPath.ViewModels;
+using Microsoft.AspNetCore.Html;
+
+namespace DFC.App.CareerPath.AutoMapperProfiles
+{
+    public class CareerPathSegmentModelProfile : Profile
+    {
+        public CareerPathSegmentModelProfile()
+        {
+            CreateMap<CareerPathSegmentModel, DocumentViewModel>()
+                .ForMember(d => d.Content, s => s.MapFrom(a => new HtmlString(a.Content)))
+                ;
+
+            CreateMap<CareerPathSegmentModel, IndexDocumentViewModel>()
+                ;
+        }
+    }
+}
