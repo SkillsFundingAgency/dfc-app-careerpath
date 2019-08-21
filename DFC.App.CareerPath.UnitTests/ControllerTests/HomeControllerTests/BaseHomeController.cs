7bcb29702a1cb46111a8d5e1dfb7c7bc34056065
Ian Crisp <icrisp@program-systems.co.uk>
2019-08-21 09:12:03 +0100

    Created unit tests for web app controller methods

---
+++ DFC.App.CareerPath.UnitTests/ControllerTests/HomeControllerTests/BaseHomeController.cs
@@ -0,0 +1,35 @@
+﻿using DFC.App.CareerPath.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+using System.Collections.Generic;
+using System.Net.Mime;
+
+namespace DFC.App.CareerPath.UnitTests.ControllerTests.HomeControllerTests
+{
+    public abstract class BaseHomeController
+    {
+        public static IEnumerable<object[]> HtmlMediaTypes => new List<object[]>
+        {
+            new string[] { "*/*" },
+            new string[] { MediaTypeNames.Text.Html },
+        };
+
+        protected HomeController BuildHomeController(string mediaTypeName)
+        {
+            var httpContext = new DefaultHttpContext();
+
+            httpContext.Request.Headers[HeaderNames.Accept] = mediaTypeName;
+
+            var controller = new HomeController()
+            {
+                ControllerContext = new ControllerContext()
+                {
+                    HttpContext = httpContext,
+                },
+            };
+
+            return controller;
+        }
+    }
+}
--- DFC.App.CareerPath.UnitTests/ControllerTests/HomeControllerTests/HomeControllerErrorTests.cs
+++ DFC.App.CareerPath.UnitTests/ControllerTests/HomeControllerTests/HomeControllerErrorTests.cs
@@ -1,13 +1,29 @@
+using DFC.App.CareerPath.ViewModels;
+using FakeItEasy;
+using Microsoft.AspNetCore.Mvc;
 using Xunit;
 
-namespace DFC.App.CareerPath.UnitTests.HomeControllerTests
+namespace DFC.App.CareerPath.UnitTests.ControllerTests.HomeControllerTests
 {
     [Trait("Home Controller", "Error Tests")]
-    public class HomeControllerErrorTests
+    public class HomeControllerErrorTests : BaseHomeController
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [MemberData(nameof(HtmlMediaTypes))]
+        public void HomeControllerErrorHtmlReturnsSuccess(string mediaTypeName)
         {
+            // Arrange
+            var expectedResult = A.Fake<ErrorViewModel>();
+            var controller = BuildHomeController(mediaTypeName);
+
+            // Act
+            var result = controller.Error();
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<ErrorViewModel>(viewResult.ViewData.Model);
+
+            controller.Dispose();
         }
     }
 }
