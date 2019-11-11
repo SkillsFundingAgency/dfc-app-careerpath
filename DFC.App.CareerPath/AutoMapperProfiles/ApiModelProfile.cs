72ab41c952b78b11fd6b72f9f6913a46c9474ea0
ilyasdhin <ilyasdhin@gmail.com>
2019-11-11 10:39:43 +0000

    Integrated html transaltor into career path (#40)
    * Integrated html transaltor into career path
    
    * Updated to latest html translator

--- DFC.App.CareerPath/AutoMapperProfiles/ApiModelProfile.cs
+++ DFC.App.CareerPath/AutoMapperProfiles/ApiModelProfile.cs
@@ -1,27 +1,16 @@
 ﻿using AutoMapper;
 using DFC.App.CareerPath.ApiModels;
 using DFC.App.CareerPath.Data.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using DFC.HtmlToDataTranslator.ValueConverters;
 
 namespace DFC.App.CareerPath.AutoMapperProfiles
 {
-    public class HtmlStringFormatter : IValueConverter<string, List<string>>
-    {
-        public List<string> Convert(string sourceMember, ResolutionContext context)
-        {
-            return new List<string> { sourceMember };
-        }
-    }
-
     public class ApiModelProfile : Profile
     {
         public ApiModelProfile()
         {
             CreateMap<CareerPathSegmentDataModel, CareerPathAndProgressionApiModel>()
-                .ForMember(d => d.CareerPathAndProgression, opt => opt.ConvertUsing(new HtmlStringFormatter(), s => s.Markup))
+                .ForMember(d => d.CareerPathAndProgression, opt => opt.ConvertUsing(new HtmlToStringValueConverter(), s => s.Markup))
                 ;
         }
     }
