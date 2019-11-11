a2f0c5758ca54525c3e314fb08c037942b52e4a0
Ilyas Dhin <ilyasdhin@gmail.com>
2019-11-11 10:26:02 +0000

    Updated to latest html translator

--- DFC.App.CareerPath/AutoMapperProfiles/ApiModelProfile.cs
+++ DFC.App.CareerPath/AutoMapperProfiles/ApiModelProfile.cs
@@ -1,27 +1,16 @@
 ﻿using AutoMapper;
 using DFC.App.CareerPath.ApiModels;
 using DFC.App.CareerPath.Data.Models;
-using DFC.HtmlToDataTranslator.Services;
-using System.Collections.Generic;
+using DFC.HtmlToDataTranslator.ValueConverters;
 
 namespace DFC.App.CareerPath.AutoMapperProfiles
 {
-    public class HtmlStringFormatter : IValueConverter<string, List<string>>
-    {
-        public List<string> Convert(string sourceMember, ResolutionContext context)
-        {
-            var translator = new HtmlAgilityPackDataTranslator();
-            var result = translator.Translate(sourceMember);
-            return result;
-        }
-    }
-
     public class ApiModelProfile : Profile
     {
         public ApiModelProfile()
         {
             CreateMap<CareerPathSegmentDataModel, CareerPathAndProgressionApiModel>()
-                .ForMember(d => d.CareerPathAndProgression, opt => opt.ConvertUsing(new HtmlStringFormatter(), s => s.Markup))
+                .ForMember(d => d.CareerPathAndProgression, opt => opt.ConvertUsing(new HtmlToStringValueConverter(), s => s.Markup))
                 ;
         }
     }
