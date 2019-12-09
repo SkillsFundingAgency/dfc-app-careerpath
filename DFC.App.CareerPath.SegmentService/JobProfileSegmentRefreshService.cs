c8e9b663d8a8d0f0a56e014fcd263d37e9b4f9a0
Ilyas Dhin <ilyasdhin@gmail.com>
2019-12-09 11:01:03 +0000

    Added correlationId to refresh message when publishing

--- DFC.App.CareerPath.SegmentService/JobProfileSegmentRefreshService.cs
+++ DFC.App.CareerPath.SegmentService/JobProfileSegmentRefreshService.cs
@@ -1,4 +1,5 @@
-﻿using DFC.App.CareerPath.Data.Contracts;
+﻿using DFC.App.CareerPath.Common.Contracts;
+using DFC.App.CareerPath.Data.Contracts;
 using Microsoft.Azure.ServiceBus;
 using Newtonsoft.Json;
 using System.Text;
@@ -9,16 +10,20 @@ namespace DFC.App.CareerPath.SegmentService
     public class JobProfileSegmentRefreshService<TModel> : IJobProfileSegmentRefreshService<TModel>
     {
         private readonly ITopicClient topicClient;
+        private readonly ICorrelationIdProvider correlationIdProvider;
 
-        public JobProfileSegmentRefreshService(ITopicClient topicClient)
+        public JobProfileSegmentRefreshService(ITopicClient topicClient, ICorrelationIdProvider correlationIdProvider)
         {
             this.topicClient = topicClient;
+            this.correlationIdProvider = correlationIdProvider;
         }
 
         public async Task SendMessageAsync(TModel model)
         {
             var messageJson = JsonConvert.SerializeObject(model);
+
             var message = new Message(Encoding.UTF8.GetBytes(messageJson));
+            message.CorrelationId = correlationIdProvider.CorrelationId;
 
             await topicClient.SendAsync(message).ConfigureAwait(false);
         }
