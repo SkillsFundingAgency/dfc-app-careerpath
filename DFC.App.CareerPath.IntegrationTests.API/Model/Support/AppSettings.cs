abb98b6f725a0fa7ad2690aed6fcfca5a0b20848
James Barnes <james@testtrends.co.uk>
2020-03-09 16:23:37 +0000

    Working test with new framework.

---
+++ DFC.App.CareerPath.IntegrationTests.API/Model/Support/AppSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace DFC.App.RelatedCareers.Tests.IntegrationTests.API.Model.Support
+{
+    public class AppSettings
+    {
+        public ServiceBusConfig ServiceBusConfig { get; set; } = new ServiceBusConfig();
+
+        public APIConfig APIConfig { get; set; } = new APIConfig();
+    }
+}
